6f4389de8fecdd846df76c39b457c515bb273e7d
ducrz <ducatolico@gmail.com>
2021-10-07 17:10:55 -0300

    Seção 03: Desenvolvimento de Aplicações Web Básico. Controllers.

--- LojaVirtual/LojaWeb/Controllers/HomeController.cs
+++ LojaVirtual/LojaWeb/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace LojaWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -28,10 +22,6 @@ namespace LojaWeb.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
