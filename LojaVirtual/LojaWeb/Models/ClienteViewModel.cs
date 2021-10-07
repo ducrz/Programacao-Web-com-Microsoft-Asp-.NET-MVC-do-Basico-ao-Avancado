e7749f5c12db45f76ab8b3df35629b8c1c89e436
ducrz <ducatolico@gmail.com>
2021-10-07 19:20:14 -0300

    Seção 3: Desenvolvimento de aplicações Web Básico. Criação de Models.

---
+++ LojaVirtual/LojaWeb/Models/ClienteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaWeb.Models
+{
+    public class ClienteViewModel
+    {
+      
+        public int Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
---
+++ LojaVirtual/LojaWeb/Models/ModeloDados/ClienteModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaWeb.Models.ModeloDados
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
