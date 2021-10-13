c5b377e13ad0b4980e3d9ef69503e89f9952d709
ducrz <ducatolico@gmail.com>
2021-10-13 15:51:19 -0300

    Seção 7: Desenvolvendo à Base de Código. Modelo de Dados.

--- LocadoraDeCarros/Dado/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Dado
-{
-    public class Class1
-    {
-    }
-}
---
+++ LocadoraDeCarros/Dado/Models/ClienteDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dados.Models
+{
+    public class ClienteDataModel{
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CNH { get; set; }
+        public string CartaoDeCredito { get; set; }
+    }
+}
